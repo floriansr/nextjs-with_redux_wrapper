0037d45ff780bb93dba3e5ba6df272587976893e
floriansr <sueurfloriansf@gmail.com>
2020-10-24 08:24:20 +0200

    [change] pages/index.js compo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
-import Page from "../components/page";
-import Link from "next/link";
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import Page from '../components/Page'
+import { wrapper } from '../redux/store'
+import { serverRenderClock, startClock } from '../redux'
 
-export default function Index() {
-  return <>
-    <Link href="/ssg">
-      <a>Static page</a>
-    </Link>
-    <Page />
-  </>
+const Index = (props) => {
+  useEffect(() => {
+    const timer = props.startClock()
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [props])
+
+  return <Page title="Index Page" linkTo="/ssr" />
 }
+
+export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
+  store.dispatch(serverRenderClock(true))
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    startClock: bindActionCreators(startClock, dispatch),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Index)
