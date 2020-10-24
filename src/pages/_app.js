d260a40391c9518f024fb427a7757e8af3bfc77e
floriansr <sueurfloriansf@gmail.com>
2020-10-24 08:23:25 +0200

    [fix] WrappedApp section

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,40 +1,8 @@
-import { Provider } from "react-redux";
-import { useStore } from "../redux/store";
+import { wrapper } from '../redux/store'
 
-const myApp = ({ Component, pageProps }) => {
-  // console.log("myApp -> pageProps", pageProps)
-  const store = useStore(pageProps.initialState);
-
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+const WrappedApp = ({ Component, pageProps }) => {
+  console.log("WrappedApp -> pageProps", pageProps)
+          return <Component {...pageProps} />
 }
 
-export default myApp
-
-
-// import { FC } from "react";
-// import { AppProps, AppContext } from "next/app";
-// import { wrapper } from "../redux/store.ts";
-
-
-// const myApp: FC<AppProps> = ({ Component, pageProps }) => {
-//   return <Component {...pageProps} />;
-// };
-
-// myApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
-//   ctx.store.dispatch(App("was set in _app"));
-
-//   return {
-//     pageProps: {
-//       ...(Component.getInitialProps
-//         ? await Component.getInitialProps(ctx)
-//         : {}),
-//       appProp: ctx.pathname,
-//     },
-//   };
-// };
-
-// export default wrapper.withRedux(myApp);
+export default wrapper.withRedux(WrappedApp)
